fbe8a8823a6cc0ee8bfd2bd59e3bdd07da55e0fc
shakiz <activities335@gmail.com>
2022-12-16 23:03:02 +0600

    Add observer for movie list and binding

--- app/src/main/java/com/example/kotlin_coroutine_mvvm/ui/MovieListActivity.kt
+++ app/src/main/java/com/example/kotlin_coroutine_mvvm/ui/MovieListActivity.kt
@@ -2,11 +2,32 @@ package com.example.kotlin_coroutine_mvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.kotlin_coroutine_mvvm.R
+import com.example.kotlin_coroutine_mvvm.databinding.ActivityMovieListBinding
 
 class MovieListActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MovieViewModel
+    lateinit var binding : ActivityMovieListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_list)
+        binding = ActivityMovieListBinding.inflate(layoutInflater)
+
+        initObjects()
+        initObserver()
+
+        viewModel.fetchAllMovieList()
+    }
+
+    private fun initObjects() {
+        viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
+    }
+
+    private fun initObserver() {
+        viewModel.movieList.observe(this){
+
+        }
     }
 }
--- app/src/main/java/com/example/kotlin_coroutine_mvvm/ui/MovieViewModel.kt
+++ app/src/main/java/com/example/kotlin_coroutine_mvvm/ui/MovieViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.kotlin_coroutine_mvvm.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.kotlin_coroutine_mvvm.data.model.Movie
 import com.example.kotlin_coroutine_mvvm.data.repository.MovieRepository
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+
+    val movieList = MutableLiveData<List<Movie>>()
 
     fun fetchAllMovieList(){
         movieRepository.fetchMovieList()

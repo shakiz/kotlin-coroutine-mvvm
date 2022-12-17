9fbf1e48efd749db5d1aff05311d075dfcd43332
shakiz <activities335@gmail.com>
2022-12-17 14:01:57 +0600

    Update movie list activity with basic api implementation

--- app/src/main/java/com/example/kotlin_coroutine_mvvm/ui/MovieListActivity.kt
+++ app/src/main/java/com/example/kotlin_coroutine_mvvm/ui/MovieListActivity.kt
@@ -5,18 +5,24 @@ import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import com.example.kotlin_coroutine_mvvm.R
+import com.example.kotlin_coroutine_mvvm.adapter.MovieAdapter
+import com.example.kotlin_coroutine_mvvm.data.datasource.MovieApi
 import com.example.kotlin_coroutine_mvvm.databinding.ActivityMovieListBinding
+import com.example.kotlin_coroutine_mvvm.service.RetrofitService
 
 class MovieListActivity : AppCompatActivity() {
 
-    lateinit var viewModel: MovieViewModel
+    private lateinit var viewModel: MovieViewModel
     lateinit var binding : ActivityMovieListBinding
+    private val movieAdapter = MovieAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMovieListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initObjects()
+        setAdapter()
         initObserver()
 
         viewModel.fetchAllMovieList()
@@ -24,11 +30,17 @@ class MovieListActivity : AppCompatActivity() {
 
     private fun initObjects() {
         viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
+        val retrofit = RetrofitService.getInstance()
+        val movieApi = MovieApi(retrofit)
+    }
+
+    private fun setAdapter(){
+        binding.rvMovie.adapter = movieAdapter
     }
 
     private fun initObserver() {
         viewModel.movieList.observe(this){
-
+            movieAdapter.setMovies(it)
         }
 
         viewModel.loading.observe(this){

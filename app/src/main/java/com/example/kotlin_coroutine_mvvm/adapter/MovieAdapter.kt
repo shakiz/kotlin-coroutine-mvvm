38f3b01bc5842fbd3bf2b3af0d03ffee96926510
shakiz <activities335@gmail.com>
2022-12-24 21:37:02 +0600

    Add glide for image loading

--- app/src/main/java/com/example/kotlin_coroutine_mvvm/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/kotlin_coroutine_mvvm/adapter/MovieAdapter.kt
@@ -3,6 +3,8 @@ package com.example.kotlin_coroutine_mvvm.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.kotlin_coroutine_mvvm.R
 import com.example.kotlin_coroutine_mvvm.data.model.Movie
 import com.example.kotlin_coroutine_mvvm.databinding.AdapterRecyclerItemMovieBinding
 
@@ -19,6 +21,9 @@ class MovieAdapter : RecyclerView.Adapter<MovieViewHolder>() {
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = movieList[position]
         holder.binding.movieName.text = movie.name.trim()
+        Glide.with(holder.itemView.context).load(movie.imageUrl)
+            .placeholder(R.drawable.placeholder)
+            .into(holder.binding.ivImageCategory)
     }
 
     override fun getItemCount(): Int {
